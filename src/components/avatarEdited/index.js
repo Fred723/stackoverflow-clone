a16fb03588922c374ccb09e64aa2cdea7e9449d9
Fred723 <frederic.nadaradjane@ynov.com>
2020-03-24 00:18:44 +0100

    logo + new component

---
+++ src/components/avatarEdited/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AvatarEdited = ({
+  size = 50,
+  src = 'https://i.stack.imgur.com/S7IWh.jpg?s=30&g=1'
+}) => {
+  return <AvatarEditedImage size={size} src={src}></AvatarEditedImage>
+}
+
+const AvatarEditedImage = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 3px;
+`
+
+export default AvatarEdited
